4cf21f21ddff9c7dc6e4a4df9dc32c5974cc6967
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-06-27 18:54:56 +0200

    Change ssl implementation

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 import fs from 'fs'
-import path from 'path'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const ip = process.env.VITE_HOST_IP_ADDRESS || 'localhost'
+const port = process.env.VITE_HOST_PORT || 5173
 
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
@@ -21,10 +21,10 @@ export default defineConfig({
   },
   server: {
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, `../../cert/${ip}-key.pem`)),
-      cert: fs.readFileSync(path.resolve(__dirname, `../../cert/${ip}.pem`)),
+      key: fs.readFileSync(process.env.VITE_SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.VITE_SSL_CERT_PATH),
     },
     host: ip,
-    port: 5173,
+    port: port,
   },
 })
