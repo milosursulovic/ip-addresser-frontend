d143374b0056a1e1c0418d73bb2e80b164dc1fe9
Misa <milosursulovic2696@gmail.com>
2025-06-27 13:13:53 +0200

    Enable ssl

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import fs from 'fs'
+import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const ip = process.env.VITE_HOST_IP_ADDRESS || 'localhost'
+
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, `../../cert/${ip}-key.pem`)),
+      cert: fs.readFileSync(path.resolve(__dirname, `../../cert/${ip}.pem`)),
+    },
+    host: ip,
+    port: 5173,
   },
 })
